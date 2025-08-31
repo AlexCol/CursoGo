31dffccef3633f66bede40c8cd7c55aee655a2f9
AlexCol <eu_axil@yahoo.com.br>
2025-08-30 23:02:30 -0300

    .

---
+++ 13.Pacotes/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Blue("Blue")
+	color.Cyan("Cyan")
+	color.Red("Red")
+
+	b := color.BlueString("BlueString")
+	r := color.RedString("RedString")
+	fmt.Println(b, r)
+
+}
