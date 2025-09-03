0bcec894468a911ab5bec2a9397996375a4792fe
AlexCol <eu_axil@yahoo.com.br>
2025-09-03 17:21:41 -0300

    .

--- 14.Goroutines/src/useMutex.go
+++ 14.Goroutines/src/useMutex.go
@@ -2,20 +2,26 @@ package src
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func UseMutex() {
 	//var wg sync.WaitGroup
 
+	var m sync.Mutex
 	i := 0
-	for x := 0; x < 100; x++ {
-		go ChangeNumber(&i, x)
+	for x := 0; x < 100000; x++ {
+		go func() {
+			m.Lock()
+			i++
+			m.Unlock()
+		}()
 	}
 
 	//wg.Wait()
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(500 * time.Millisecond)
 	fmt.Printf("Valor i é: %d", i)
 }
 
